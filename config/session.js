36c99aec096c6a9e73a064705f35ef3a58066a68
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-09-13 21:15:14 +0200

    last changes

--- config/session.js
+++ config/session.js
@@ -7,16 +7,17 @@ app.use(
     session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
 
+    store: new MongoStore({
+        mongoUrl: process.env.DB_URI || "mongodb://localhost/sharea",
+        ttl: 60 * 60 * 24
+    }),
     cookie: {
         maxAge: 1000 * 60 * 60 * 24
       },
     
-    store: new MongoStore({
-        mongoUrl: process.env.DB_URI || "mongodb://localhost/sharea",
-        ttl: 60 * 60 * 24
-    })
+   
 }))
 
 }
--- routes/api.routes.js
+++ routes/api.routes.js
@@ -4,13 +4,13 @@ const saltRounds = 10;
 const User = require("../models/User.model");
 const Image = require("../models/Image.model")
 const uploadCloud = require("../config/cloudinary");
+const { get } = require("mongoose");
 
 
 // Sign Up
 router.post("/signup", (req, res) =>{
      console.log("Hi there ", req.body)
     const {username, password, email} = req.body;
-
     bcrypt
         .genSalt(saltRounds)
         .then( (salt) => bcrypt.hash(password, salt) )
@@ -26,36 +26,44 @@ router.post("/signup", (req, res) =>{
 // Sign In
 router.post("/signin", (req, res) =>{
     const {username, password} = req.body;
-    User.find({username})
-    .then(user =>{
-            console.log(req.body, user)
-                
-            if( bcrypt.compareSync(password, user.password) ){
-                req.session.currentUser = user;
-                res.status(200).json(req.session);
+  
+    User.findOne({username})
+        .then(username =>{
+            if( bcrypt.compareSync(password, username.password) ){
+                req.session.currentUser = username;
+                console.log(req.session)
+                res.status(200).json(username);
                 // console.log(req.session)
             }
             else{ console.log('false')}
         } )
+        .catch(err => console.log(err))
     })
 
 // Log Out
 router.post("/logout", (req, res) =>{
-    res.clearCookie("connect.sid");
+    console.log(req.session)
+    //res.clearCookie("connect.sid");
     req.session.destroy();
     console.log('session destroyed:', req.session)
 })
 
 // Get Auth
 router.get("/auth" , (req, res) =>{
-    console.log('hi')
-    if(req.session){
-        console.log(req.session)
-        return res.status(200).json(req.session.currentUser);
-    }
-    else{
-        return 
-    }
+
+    console.log(req.session)
+    res.status(200).json(req.session.currentUser);
+   
+})
+
+router.get("/auth/:id", (req, res) =>{
+    const {id} = req.params;
+    User.findById(id)
+        .then(response =>{
+            req.session.currentUser = response;
+            res.json(response)
+        })
+        .catch(err => console.log(err));
 })
 
 // Get Images
